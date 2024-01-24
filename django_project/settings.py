a24aae32e0bef46695a1dfc880969e0d20bb44aa
ColinAdo <colinadodev30@gmail.com>
2024-01-24 18:51:34 +0300

    Add apis app and rest_framework

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party 
+    "rest_framework",
+
+    # Local
     "books.apps.BooksConfig",
+    "apis.apps.ApisConfig",
 ]
 
 MIDDLEWARE = [
